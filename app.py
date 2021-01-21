5b44c9abc5a445db5f9688da362f65c855b82270
Unkei Kou <unkei.kou.chs@gmail.com>
2021-01-20 19:33:50 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import setup_db
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  CORS(app)
+
+  @app.route('/')
+  def welcome():
+  return 'Welcome to the empty main page!'
+
+
+  return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
