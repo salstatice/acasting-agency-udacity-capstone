c70c9064f832c1d03b5052d02939e9d75b228914
Unkei Kou <unkei.kou.chs@gmail.com>
2021-01-21 12:40:11 -0500

    Initial database migration

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 database_path = os.environ['DATABASE_URL']
@@ -16,7 +17,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate = Migrate(app, db)
 
 
 '''
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-export DATABASE_URL="postgres://postgres@localhost:5432/acasting"
+database='acasting'
+export DATABASE_URL="postgres://postgres:postgres@localhost:5432/$database"
 
