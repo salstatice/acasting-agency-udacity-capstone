dbabdd847a45e1788791827db6fe856a6a549c58
Unkei Kou <unkei.kou.chs@gmail.com>
2021-01-22 15:08:49 -0500

    Change spacing/tab size

--- auth.py
+++ auth.py
@@ -5,48 +5,48 @@ from functools import wraps
 
 
 class AuthError(Exception):
-  def __init__(self, error, status_code):
-    self.error = error
-    self.status_code = status_code
+	def __init__(self, error, status_code):
+		self.error = error
+		self.status_code = status_code
 
 ## Auth Header
 
 def get_token_check_auth_header():
-  '''
-  Check if request has the appropiate header
+	'''
+	Check if request has the appropiate header
     raise an AuthError if
-    - 'Authorization' not present in header
-    - a malformed token present under 'Authorization'
+    	- 'Authorization' not present in header
+    	- a malformed token present under 'Authorization'
 
     it split bearer and the token, and
     return the token part of the header
-  '''
-  # check for Auth in header
+	'''
+	# check for Auth in header
 
-  if 'Authorization' not in request.headers:
-    raise AuthError({
-      'code': 'authorization_header_missing',
-      'description': 'Authorication header is expected'
+	if 'Authorization' not in request.headers:
+		raise AuthError({
+			'code': 'authorization_header_missing',
+			'description': 'Authorication header is expected'
     }, 401)
 
   # check for bearer token header
 
-  auth_header = request.headers['Authorization']
-  header_parts = auth_header.split(' ')
+	auth_header = request.headers['Authorization']
+	header_parts = auth_header.split(' ')
   
-  if len(header_parts) != 2:
-    raise AuthError({
-      'code':'invalid_authorization_header',
-      'description': 'Expecting bearer token.'
-    }, 401)
-  elif header_parts[0].lower() != 'bearer':
-    raise AuthError({
-      'code':'invalid_authorization_header',
-      'description': 'Authorization header must start with "Bearer".'
-    }, 401)
+	if len(header_parts) != 2:
+		raise AuthError({
+			'code':'invalid_authorization_header',
+			'description': 'Expecting bearer token.'
+		}, 401)
+	elif header_parts[0].lower() != 'bearer':
+		raise AuthError({
+			'code':'invalid_authorization_header',
+			'description': 'Authorization header must start with "Bearer".'
+		}, 401)
 
-  # return token
-  return header_parts[1]
+	# return token
+	return header_parts[1]
 
 
 def requires_auth(permissions=''):
